8d0374c3bf1e15db15f5f6e513ba05cb95c051bb
RobertGallegos100 <teto.fuu3@gmail.com>
2016-12-03 17:08:00 -0700

    Initial commit

---
+++ app/src/main/java/com/robertg/tecii/cuentapasos/CuentaPAsos.java
@@ -0,0 +1,63 @@
+package com.robertg.tecii.cuentapasos;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CuentaPAsos extends AppCompatActivity implements SensorEventListener {
+
+
+
+    SensorManager sensorManager;
+
+    TextView tv_steps;
+
+    boolean running = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuenta_pasos);
+        tv_steps = (TextView) findViewById(R.id.tv_steps);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor != null){
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+
+        } else {
+            Toast.makeText(this, "Sensor no encontrado", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (running) {
+            tv_steps.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
